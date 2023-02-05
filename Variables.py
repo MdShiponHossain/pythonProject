a2b5512000bd0e2a06270cdb91ac618ad3465503
Shipon Hossain <engr.shiponhossain@gmail.com>
2023-02-05 19:49:56 +0600

    Variables

---
+++ Variables.py
@@ -0,0 +1,8 @@
+Name= "Shipon Hossain"
+age= "22"
+CGPA= "3.80"
+
+print("Student Name is " +Name)
+print(Name + " live in Dhaka")
+print("He is "+ age +" years old")
+print(Name + " has scorde "+ CGPA +" ")
