51c15557316562f71a91e602c5e263645151563d
Shipon Hossain <engr.shiponhossain@gmail.com>
2023-02-05 22:05:32 +0600

    Variables

--- Variables.py
+++ Variables.py
@@ -2,7 +2,7 @@ Name= "Shipon Hossain"
 age= "22"
 CGPA= "3.80"
 
-print("Student Name is " +Name)
-print(Name + " live in Dhaka")
-print("He is "+ age +" years old")
+print("Student Name is " +Name )
+print(Name + " live in Dhaka.")
+print("He is "+ age +" years old.")
 print(Name + " has scorde "+ CGPA +" ")
