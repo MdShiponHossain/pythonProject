89f0374ad25724a59ec58d0fa00b60cb94e887ca
Shipon Hossain <engr.shiponhossain@gmail.com>
2023-02-06 01:03:48 +0600

    Type Casting

---
+++ program 3.py
@@ -0,0 +1,9 @@
+
+num1 = input("Enter the number : ")
+num2 = input("Enter the number : ")
+
+result = int(num1) + int(num2)
+print("Enter result : ", result)
+
+result = int(num1) - int(num2)
+print("Enter result : ", result)
