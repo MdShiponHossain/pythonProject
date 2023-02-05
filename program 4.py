a3a913544d49c4f0eeb592ec3b124f1ceb60dfe1
Shipon Hossain <engr.shiponhossain@gmail.com>
2023-02-06 01:04:31 +0600

    Type Casting

---
+++ program 4.py
@@ -0,0 +1,14 @@
+num1 = int (input("Enter the number : "))
+num2 = int (input("Enter the number : "))
+
+result = num1 + num2
+print("Enter result : ", result)
+
+result = num1 - num2
+print("Enter result : ", result)
+
+result = num1 * num2
+print("Enter result : ", result)
+
+result = num1 / num2
+print("Enter result : ", result)
