327898819d241bd91d9484dc4a35384faf5b66e4
Shipon Hossain <engr.shiponhossain@gmail.com>
2023-02-11 23:45:20 +0600

    elif statement

---
+++ program 13.py
@@ -0,0 +1,22 @@
+#Mark
+
+mark = 80
+
+if mark >=80:
+    print("A+")
+
+elif mark >= 70:
+    print("B+")
+
+elif mark >= 60:
+    print("B")
+
+elif mark >= 50:
+    print("C")
+
+elif mark >= 40:
+    print("D")
+
+else:
+    print("Fail")
+
