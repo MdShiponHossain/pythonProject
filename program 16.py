87db20df8d27106dd3fa92720e75079f8d3a9bc3
Shipon Hossain <engr.shiponhossain@gmail.com>
2023-02-12 22:56:47 +0600

    Logical operator

---
+++ program 16.py
@@ -0,0 +1,14 @@
+# Logical operator are And,Or, Not
+
+num1 = 30
+num2 = 40
+num3 = 10
+
+if num1 > num2 and num1 > num3 :
+    print("num1")
+
+elif num2 > num1 and num2 > num3:
+    print("num2")
+
+else:
+    print("num3")
