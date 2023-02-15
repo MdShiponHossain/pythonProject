0460dd1da6b5a9577b5d1cdea49ad6c19456d76e
Shipon Hossain <engr.shiponhossain@gmail.com>
2023-02-16 01:38:47 +0600

    Series

--- program 31.py
+++ program 31.py
@@ -1,8 +1,8 @@
-# 1 + 2 + 3 + 4 + 5 + 6 +...+ n
+# 1*1 + 2*2 + 3*3 +...+ n
 
 n = int(input("Enter the last number : "))
 sum = 0
 
 for x in range (1, n+1, 1):
-    sum = sum + x
+    sum = sum + x*x
 print(sum)
