bb1f1b84da3750b5045700a719eab98e58212686
Shipon Hossain <engr.shiponhossain@gmail.com>
2023-05-05 22:11:24 +0600

    class and object

---
+++ program 78.py
@@ -0,0 +1,13 @@
+class student:
+    roll: ""
+    gpa: ""
+
+shipon = student()
+shipon.roll = 206
+shipon.gpa = 3.90
+print(f"Roll : {shipon.roll}, GPA : {shipon.gpa}")
+
+mariya = student()
+mariya.roll = 264
+mariya.gpa = 3.50
+print(f"Roll : {mariya.roll}, GPA : {mariya.gpa}")
